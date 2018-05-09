4b5c933d73c4e4cf6d508fca4a901ac07c6c8766
dylancfe15 <dylancfe15@gmail.com>
2018-05-08 20:57:10 -0400

    Initial commit

---
+++ Termi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Termi
+//
+//  Created by Difeng Chen on 5/8/18.
+//  Copyright © 2018 Difeng Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
