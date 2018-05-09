594510ab3c0063f57adf802ccc7364307a3f1973
dylancfe15 <dylancfe15@gmail.com>
2018-05-08 21:06:58 -0400

    firebase

---
+++ Pods/Target Support Files/Pods-Termi/Pods-Termi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Termi : NSObject
+@end
+@implementation PodsDummy_Pods_Termi
+@end
---
+++ Pods/Target Support Files/Pods-TermiTests/Pods-TermiTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TermiTests : NSObject
+@end
+@implementation PodsDummy_Pods_TermiTests
+@end
